a4b389337e0ea6ba948be0c42d9a556d03aa3169
PelicanSkeleton <sgphillips80@gmail.com>
2020-02-15 11:19:06 -0500

    Need to figure out why my add, delete and updates are not working.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (name, devoured) VALUE ("She's a superLEEK", false);
+INSERT INTO burgers (name, devoured) VALUE ("I know why the cajun burger sings", false);
+INSERT INTO burgers (name, devoured) VALUE ("We're here, we're gruyere, get used to it", false);
+INSERT INTO burgers (name, devoured) VALUE ("Pepper Don't Preach", false);
